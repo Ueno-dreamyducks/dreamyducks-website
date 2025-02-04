eb889532d203ed20c17bfe112f3e3a226519a226
Ueno-dreamyducks <taku0katy@duck.com>
2025-02-04 00:28:31 -0600

    Initialize App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import './themeColor.css';
+import githubIcon from './images/github-mark.svg';
 
 function App() {
   return (
     <div className="App">
+      <div className='Top-banner'>
+        <h1>dreamyducks</h1>
+      </div>
+      <div className='Top-banner-links'>
+        <div className='top-banner-link-container' onClick={() => { window.location.href = 'https://github.com/Ueno-dreamyducks'; }}>
+          <div style={{display: "flex", widows: "100%"}} className='padding-horizontal-16'>
+            <img src={githubIcon} alt='Github icon' style={{objectFit: "contain"}} />
+            <div>
+              <h1>Visit our Github</h1>
+              <p>View our open codes</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+      <div className='Homepage'>
+        <div className='padding-16'>
+          <h1 className='no-margin'>aa</h1>
+        </div>
+      </div>
     </div>
   );
 }
