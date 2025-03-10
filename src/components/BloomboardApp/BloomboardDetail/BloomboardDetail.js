29b5893196aa688b99a458242bccf9d8a05577b8
Ueno_dreamyducks <taku0katy@duck.com>
2025-03-10 17:13:53 -0500

    bloomboard class detail desighn

--- src/components/BloomboardApp/BloomboardDetail/BloomboardDetail.js
+++ src/components/BloomboardApp/BloomboardDetail/BloomboardDetail.js
@@ -1,4 +1,6 @@
 import { useParams } from "react-router-dom";
+import './Bloomboard.css';
+import { ArrowBack } from "@mui/icons-material";
 
 function BloomboardDetail() {
     const { key } = useParams();
@@ -6,12 +8,26 @@ function BloomboardDetail() {
     const data = JSON.parse(sessionStorage.getItem(key));
 
     return (
-        <div>
+        <div className="BBDetail-container">
+            <div className="BBDetail-top">
+                <ArrowBack sx={{fontSize: "48px", padding: "0 12px", width: "5%", cursor:"pointer"}} />
+                <h1 className="text-bold text-align-center" style={{width: "95%"}}>{data.className}</h1>
+            </div>
             {data.assignments.map((assignment, index) => (
-                <p key={index}>{assignment.Assignment}</p>
+                <BBDetail key={index} row={assignment} />
             ))}
         </div>
     )
 }
 
 export default BloomboardDetail;
+
+function BBDetail({row}) {
+    return(
+        <div className="BB-row">
+            <div className="BB-row-content">
+            <p style={{width: "80%"}} >{row.Assignment.slice(0 , -1)}</p>
+            <p style={{width: "20%"}} className="BBDetail-row-score">{row.Score}</p>            </div>
+        </div>
+    )
+}
